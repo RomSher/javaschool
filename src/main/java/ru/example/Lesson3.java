675677b910c4821e8f9891d9a61ca7fa8cff7498
Roman <72413159+RomSher@users.noreply.github.com>
2020-11-09 23:10:49 +0500

    lesson6 done

--- src/main/java/ru/example/Lesson3.java
+++ src/main/java/ru/example/Lesson3.java
@@ -3,10 +3,7 @@ package ru.example;
 import java.util.stream.IntStream;
 
 public class Lesson3 {
-    public static void main(String[] args) {
-
-    }
-    /*
+     /*
     Написать публичный метод принимающий 2 параметра: первый типа byte и второй типа short.
     Метод должен возвращать сумму значений параметров. Тип возвращаемого значения должен быть byte.
     */
